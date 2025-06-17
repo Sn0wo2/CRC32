9db36b1a766fbb651cb32479a129eba767e2ed29
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-06-17 15:49:06 +0800

    fix: example

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,13 @@
 import impl.CRC32
+import `interface`.ICRC32
 
 fun main() {
     val hash = ""
+    val crc32: ICRC32 = CRC32()
+
     val start = System.currentTimeMillis()
-    val result = CRC32().reverseMID(hash)
+    val result = crc32.reverseMID(hash)
+
     val elapsed = System.currentTimeMillis() - start
     println("Hash: $hash")
     println("Result: $result")
